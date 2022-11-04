496c64360d0e8f6f79e214fd9d4e39ef9fd5a130
Dom Rodriguez <shymega@shymega.org.uk>
2022-11-04 18:03:02 +0000

    Make Lambda handler return statusCodes, and handle errors better

--- cloud_gaming_telegram_bot/app.py
+++ cloud_gaming_telegram_bot/app.py
@@ -2,21 +2,24 @@ import json
 from utils.telegram import Telegram
 
 
-def telegram_handler(evt: dict):
+def telegram_handler(evt: dict) -> dict:
     t = Telegram()
     body = json.loads(evt["body"])
 
     t.handle_update(body)
 
+    return {"statusCode": 200}
+
 
 def lambda_handler(evt: dict, _ctx: dict) -> dict:
-    if "message" in json.loads(evt["body"]):
-        # This is a call from Telegram
-        telegram_handler(evt)
-    elif "state" in json.loads(evt["body"])["payload"]:
-        # This is a call from the Azure VM, but this isn't coded yet! TODO.
-        return {"statusCode": 400}
-    else:
-        pass
+    try:
+        if "message" in json.loads(evt["body"]):
+            # This is a call from Telegram
+            return telegram_handler(evt)
+        if "state" in json.loads(evt["body"])["payload"]:
+            # This is a call from the AWS VM, but this isn't coded yet! TODO.
+            return {"statusCode": 404}
+    except KeyError:
+        return {"statusCode": 500}
 
     return {"statusCode": 200}
