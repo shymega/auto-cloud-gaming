9a19d6899c59d00122fe300a4e7131bae1f08263
Dom Rodriguez <shymega@shymega.org.uk>
2022-11-04 15:53:32 +0000

    Revamp LambdaConfiguration class

--- cloud_gaming_telegram_bot/utils/lambda_configuration.py
+++ cloud_gaming_telegram_bot/utils/lambda_configuration.py
@@ -14,18 +14,34 @@ class LambdaConfiguration:
         try:
             return getenv("LAMBDA_FUNC_TELEGRAM_TOKEN")
         except KeyError:
-            raise SystemError("We have no Telegram token! Bailing...")
+            # Return None, as we can't get the env var
+            return None
 
     @staticmethod
-    def get_azure_compute_token() -> Optional[str]:
+    def get_aws_access_tokens() -> dict[str, Optional[str]]:
+        response_dict = {}
+
         try:
-            return getenv("LAMBDA_FUNC_AZURE_COMPUTE_TOKEN")
+            response_dict["aws_access_key_id"] = getenv("LAMBDA_FUNC_AWS_ACCESS_KEY_ID")
         except KeyError:
-            raise SystemError("We have no Azure Compute token! Bailing...")
+            # Set value of key to None, as we can't get the env var
+            response_dict["aws_access_key_id"] = None
+
+        try:
+            response_dict["aws_secret_access_key"] = getenv(
+                "LAMBDA_FUNC_AWS_SECRET_ACCESS_KEY"
+            )
+        except KeyError:
+            # in future, return graceful errror
+            # Set value of key to None, as we can't get the env var
+            response_dict["aws_secret_access_key"] = None
+
+        return response_dict
 
     @staticmethod
     def get_telegram_owner_id() -> Optional[str]:
         try:
             return getenv("LAMBDA_FUNC_TELEGRAM_OWNER_ID")
         except KeyError:
-            raise SystemError("We have no targeted Telegram user ID! Bailing...")
+            # Return None, as we can't get the env var
+            return None
